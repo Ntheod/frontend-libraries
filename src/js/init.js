9b8e4a5e18d790007475d4914cc89be8a55c3e1d
Nikolaos Theodorou <ntheodorou@product-me.eu>
2025-06-18 08:28:39 +0000

    first testing commit

---
+++ src/js/init.js
@@ -0,0 +1,4 @@
+function onLoad() { //initial function here we handle any calls, want to be responsive, hide elements etc etc.
+    console.log("We loaded successfully")
+    chooseTransition("fade", "in", "main-container", false);
+}
